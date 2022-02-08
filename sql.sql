dbd4a93d357a5ed8b17c43f1abf4284daa216516
pooriaahmadi <pooriababak444@gmail.com>
2022-02-08 14:46:48 -0500

    sql update

--- sql.sql
+++ sql.sql
@@ -2,10 +2,10 @@
 -- version 5.1.1
 -- https://www.phpmyadmin.net/
 --
--- Host: 127.0.0.1
--- Generation Time: Jan 16, 2022 at 06:18 PM
+-- Host: localhost
+-- Generation Time: Feb 08, 2022 at 08:46 PM
 -- Server version: 10.4.22-MariaDB
--- PHP Version: 8.1.0
+-- PHP Version: 8.0.15
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -18,23 +18,42 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `family_everywhere`
+-- Database: `manto`
 --
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `folders`
+-- Table structure for table `teams`
 --
 
-CREATE TABLE `folders` (
+CREATE TABLE `teams` (
   `id` int(11) NOT NULL,
-  `user` int(11) NOT NULL,
   `name` varchar(191) NOT NULL,
-  `slug` varchar(191) NOT NULL,
-  `mode` tinyint(4) NOT NULL,
-  `password` varchar(191) NOT NULL,
-  `salt` varchar(191) NOT NULL
+  `user` int(11) NOT NULL,
+  `description` text DEFAULT NULL,
+  `avatar` varchar(191) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL,
+  `username` varchar(191) NOT NULL,
+  `email` varchar(191) NOT NULL,
+  `permissions` int(11) NOT NULL DEFAULT 0,
+  `salt` varchar(191) NOT NULL,
+  `hashed_password` varchar(191) NOT NULL,
+  `token` varchar(191) NOT NULL,
+  `verified` tinyint(1) NOT NULL DEFAULT 0,
+  `verification_token` varchar(191) DEFAULT NULL,
+  `preferred_name` varchar(191) DEFAULT NULL,
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
+  `updated_at` timestamp NOT NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
@@ -42,19 +61,36 @@ CREATE TABLE `folders` (
 --
 
 --
--- Indexes for table `folders`
+-- Indexes for table `teams`
 --
-ALTER TABLE `folders`
+ALTER TABLE `teams`
   ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `username` (`username`),
+  ADD UNIQUE KEY `email` (`email`),
+  ADD UNIQUE KEY `salt` (`salt`),
+  ADD UNIQUE KEY `token` (`token`),
+  ADD UNIQUE KEY `verification_token` (`verification_token`);
 
 --
 -- AUTO_INCREMENT for dumped tables
 --
 
 --
--- AUTO_INCREMENT for table `folders`
+-- AUTO_INCREMENT for table `teams`
 --
-ALTER TABLE `folders`
+ALTER TABLE `teams`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 COMMIT;
 
