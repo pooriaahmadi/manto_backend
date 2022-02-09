cd4cc44b90233f2633578e7cd91c62b4eab872b6
pooriaahmadi <pooriababak444@gmail.com>
2022-02-09 15:14:21 -0500

    members route

---
+++ src/apps/teams/routes/members.ts
@@ -0,0 +1,19 @@
+import AppRoute from "../../../classes/AppRoute";
+import { Response } from "express";
+import { Request } from "../../../types/interfaces";
+import AuthorizationToken from "../../../middlewares/AuthorizationToken";
+import Teams from "../../../classes/Teams";
+
+const execute = async (request: Request, response: Response) => {
+  const team = await Teams.getById(parseInt(request.params.id));
+  if (!team) return response.status(404).json({ message: "Team not found" });
+  const teamMembers = (await team.getMembers()).map((item) => item.toJSON());
+  return response.json({ ...team.toJSON(), members: teamMembers });
+};
+
+export default new AppRoute({
+  execute: execute,
+  method: "GET",
+  customRoute: ":id/members",
+  middlewares: [AuthorizationToken],
+});
