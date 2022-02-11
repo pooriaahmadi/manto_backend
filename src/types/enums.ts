071a196cae803bce2e17b9a2e9d5e8b2e3f6d9df
pooriaahmadi <pooriababak444@gmail.com>
2022-02-11 15:46:34 -0500

    enums update

--- src/types/enums.ts
+++ src/types/enums.ts
@@ -1,13 +1,18 @@
 export enum logMethods {
-    INFO = 1,
-    ERROR = 2,
-    LOAD = 3,
-    DEBUG = 4,
-    READY = 5
+  INFO = 1,
+  ERROR = 2,
+  LOAD = 3,
+  DEBUG = 4,
+  READY = 5,
 }
 
 export enum FolderModes {
-    PUBLIC = 0,
-    PRIVATE = 1,
-    PASSWORD = 2,
+  PUBLIC = 0,
+  PRIVATE = 1,
+  PASSWORD = 2,
+}
+export enum CommentModes {
+  NEUTRAL = 0,
+  POSITIVE = 1,
+  NEGATIVE = 2,
 }
